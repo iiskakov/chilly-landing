b6030532f5f68dae60d92f2875c7d0d7b6c84d38
Iskander Iskakov <tradeshells@gmail.com>
2021-11-04 02:45:14 +0600

    Pacman eats projects

--- components/blocks.js
+++ components/blocks.js
@@ -10,6 +10,11 @@ export default function Blocks() {
           id={styles.scrollContainer}
           className="h-32 flex-1 border-t-2 border-b-2 md:border-t-4 md:border-b-4 border-black flex justify-end content-end flex-col"
         >
+          <div className={styles.pacman}>
+            <div className={styles.pacmanTop}></div>
+            <div className={styles.pacmanBottom}></div>
+          </div>
+
           <div className="space-x-2 whitespace-nowrap" id={styles.scrollText}>
             {projects.map((project) => (
               <span className=" text-xl md:text-2xl font-bold tracking-tighter leading-5">
@@ -20,7 +25,7 @@ export default function Blocks() {
         </div>
         <div className=" border-l-2 md:border-l-4 border-black h-28"></div>
         <div className="h-32 flex-1 border-t-2 border-b-2 md:border-t-4 md:border-b-4 border-black">
-          <h1 className="text-xs md:text-2xl font-bold tracking-tighter">
+          <h1 className="text-xs md:text-sm  font-bold tracking-tighter">
             производим:
             <br />
             рекламу, клипы, ролики, 3d
--- components/footer.js
+++ components/footer.js
@@ -5,14 +5,9 @@ export default function Footer() {
   return (
     <footer className="">
       <Container>
-        <div className={styles.pacman}>
-          <div className={styles.pacmanTop}></div>
-          <div className={styles.pacmanBottom}></div>
-          <div className={styles.feed}></div>
-        </div>
-        {/* <span className="text-base lg:text-1xl font-bold tracking-tighter leading-6 text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2"> */}
-        {/*   чилли продакшн */}
-        {/* </span> */}
+        <span className="text-base lg:text-1xl font-bold tracking-tighter leading-6 text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+          чилли продакшн
+        </span>
       </Container>
     </footer>
   );
