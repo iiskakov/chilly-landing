ffac4372572d35a57c1f8a0ec984d75165efc626
Iskander Iskakov <tradeshells@gmail.com>
2021-11-04 02:11:20 +0600

    init

---
+++ components/blocks.js
@@ -0,0 +1,32 @@
+import styles from "./blocks.module.css";
+
+export default function Blocks() {
+  const projects = ["сбер", "магнум", "bi group", "beeline", "forte bank"];
+
+  return (
+    <>
+      <section className="md:flex-row flex space-x-1 md:space-x-2 items-center md:justify-between mt-2 mb-16 md:mb-12">
+        <div
+          id={styles.scrollContainer}
+          className="h-32 flex-1 border-t-2 border-b-2 md:border-t-4 md:border-b-4 border-black flex justify-end content-end flex-col"
+        >
+          <div className="space-x-2 whitespace-nowrap" id={styles.scrollText}>
+            {projects.map((project) => (
+              <span className=" text-xl md:text-2xl font-bold tracking-tighter leading-5">
+                {project}
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className=" border-l-2 md:border-l-4 border-black h-28"></div>
+        <div className="h-32 flex-1 border-t-2 border-b-2 md:border-t-4 md:border-b-4 border-black">
+          <h1 className="text-xs md:text-2xl font-bold tracking-tighter">
+            производим:
+            <br />
+            рекламу, клипы, ролики, 3d
+          </h1>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ components/description.js
@@ -0,0 +1,12 @@
+export default function Description() {
+  return (
+    <>
+      <section className="flex-col md:flex-row flex  md:justify-between mt-24">
+        <h1 className="text-2xl md:text-4xl font-bold tracking-tighter leading-6">
+          мы продакшн,
+          <br /> работаем на результат
+        </h1>
+      </section>
+    </>
+  );
+}
