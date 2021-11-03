36429354fd5cac55d33685f9509687408a57d634
Iskander Iskakov <tradeshells@gmail.com>
2021-11-04 03:36:51 +0600

    only the telegram name is blue now

--- components/footer.js
+++ components/footer.js
@@ -5,8 +5,10 @@ export default function Footer() {
   return (
     <footer>
       <Container>
-        <span className="hover:underline text-blue-400 text-base lg:text-1xl font-bold tracking-tighter leading-6 text-right w-screen lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-          <a href="https://t.me/victorbrodyagin">→ написать в телеграм</a>
+        <span className="hover:underline text-base lg:text-1xl font-bold tracking-tighter leading-6 text-right w-screen lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+          <a href="https://t.me/victorbrodyagin">
+            → написать в <span className="text-blue-400">телеграм</span>
+          </a>
         </span>
         <div class="border-t-2 border-black md:border-t-4"></div>
       </Container>
