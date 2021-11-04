5eaab90ba97e024d3741524c8ab6e36da0d6be4d
Iskander Iskakov <tradeshells@gmail.com>
2021-11-04 15:34:30 +0600

    Footer ??? pls answer

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer>
       <Container>
-        <span className="hover:underline text-base lg:text-1xl font-bold tracking-tighter leading-6 text-right w-screen lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+        <span className="hover:underline text-base lg:text-1xl font-bold tracking-tighter leading-6 text-right w-screen lg:text-left mb-0 lg:mb-0 lg:pr-4 lg:w-1/2">
           <a href="https://t.me/victorbrodyagin">
             → написать в <span className="text-blue-400">телеграм</span>
           </a>
