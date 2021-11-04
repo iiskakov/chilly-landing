9edcd1f7de1ca56825c3ee9d31221692a49d0fa8
Iskander Iskakov <tradeshells@gmail.com>
2021-11-04 16:09:06 +0600

    damn i'm high... this height..

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,35 @@
 import Alert from "../components/alert";
 import Footer from "../components/footer";
 import Meta from "../components/meta";
+import {useEffect} from "react";
 
 export default function Layout({preview, children}) {
+  let vh;
+  useEffect(() => {
+    // only execute all the code below in client side
+    if (typeof window !== "undefined") {
+      // Handler to call on window resize
+      function handleResize() {
+        vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+      }
+
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+
+      console.log(vh);
+
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
   return (
     <>
       <Meta />
-      <div style={{minHeight: "80vh"}}>
+      <div className="min-h-screen">
         <main>{children}</main>
       </div>
       <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,19 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    height: (theme) => ({
+      auto: "auto",
+      ...theme("spacing"),
+      full: "100%",
+      screen: "calc(var(--vh) * 100)",
+    }),
+    minHeight: (theme) => ({
+      0: "0",
+      ...theme("spacing"),
+      full: "100%",
+      screen: "calc(var(--vh) * 80)",
+    }),
     extend: {
-      minHeight: {
-        0: "0",
-        "1/4": "25%",
-        "1/2": "50%",
-        "3/4": "75%",
-        full: "100%",
-      },
       colors: {
         "accent-1": "#FAFAFA",
         "accent-2": "#EAEAEA",
