c06e724ad43f41fbafe4451cc670510ffd18400c
Iskander Iskakov <tradeshells@gmail.com>
2021-11-04 21:34:44 +0600

    Свой устав монастырь вот это все

--- components/intro.js
+++ components/intro.js
@@ -4,7 +4,7 @@ export default function Intro() {
       <div className="border-t-2 border-black mt-2 md:border-t-4"></div>
       <section className="flex-col md:flex-row flex md:justify-between mb-12 md:mb-12">
         <h1 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight">
-          чилли <small>🌶️</small>
+          chilly production <small>🌶️</small>
         </h1>
       </section>
     </>
