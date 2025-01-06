dc3c375c409671a39f54fb7062c6cd22cbf7aa95
Iskander <tradeshells@gmail.com>
2025-01-06 12:59:13 +0500

    about

--- pages/about/index.js
+++ pages/about/index.js
@@ -4,18 +4,16 @@ const AboutPage = () => {
       {/* Full Screen Image */}
       <div className="absolute inset-0">
         <img
-          src="https://res.cloudinary.com/dekfylowi/image/upload/f_auto,q_auto/kkjuybo82bsgztkg0by3" // Replace this with the actual URL of the team image
+          src="https://res.cloudinary.com/dekfylowi/image/upload/v1736148456/brbew1vkgsvo4wju94ry.webp" // Replace this with the actual URL of the team image
           alt="Team"
-          className="w-full h-full object-cover filter grayscale"
+          className="w-full h-full object-cover"
         />
       </div>
 
       {/* Overlay Content */}
       <div className="relative z-2 flex flex-col justify-center items-center h-full bg-black bg-opacity-50">
         <h1 className="text-white text-5xl font-bold mb-4">Chilly Production Team</h1>
-        <p className="text-white text-lg mb-8 max-w-[80vw] md:max-w-xl text-center">
-          We are a dynamic and innovative team, committed to creating extraordinary productions and experiences. Our passion for storytelling and creativity drives us to deliver high-quality projects in every aspect of our work.
-        </p>
+        <p className="text-white text-lg mb-8 max-w-[80vw] md:max-w-xl text-center">Chilly Production is a production house based in Almaty, Kazakhstan, established in 2020. We specialize in creating unique video projects with style and personality. From vibrant music videos and captivating commercials to short films and engaging vlogs, we bring stories to life that inspire and leave a lasting impression.</p>
 
         {/* Instagram Link */}
         <a
